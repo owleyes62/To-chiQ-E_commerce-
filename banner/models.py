712ece90ef368b2aad4353ce8fc037054b12fb09
Iwerson <guilhermepdo2012@gmail.com>
2023-12-15 22:48:38 -0300

    Criando resumo da compra

--- banner/models.py
+++ banner/models.py
@@ -7,10 +7,6 @@ class Banner(models.Model):
 
     def image_tag_path(self):
         return mark_safe('<img src= "%s" width="100"/>' % (self.imagem.url))
-    
-    class Meta: 
-        verbose_name_plural = '1. banners'
-
 
     def __str__(self):
         return self.alt_text
--- produto/views.py
+++ produto/views.py
@@ -166,7 +166,19 @@ class Carrinho(View):
         return render(self.request, 'produto/carrinho.html' ,contexto)
 
 class ResumoDaCompra(View):
-    pass
+    def get(self, *args, **kwargs):
+
+        if not self.request.user.is_authenticated:
+            return redirect('perfil:criar')
+
+
+        contexto = {
+            'usuario' : self.request.user,
+            'carrinho' : self.request.session['carrinho'],
+
+        }
+        
+        return render(self.request, 'produto/resumodacompra.html', contexto)
 
 class Busca(View):
     pass
