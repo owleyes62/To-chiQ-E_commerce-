34f8dc4f019097a00da07f4b52843843d59263c2
Iwerson <guilhermepdo2012@gmail.com>
2023-12-15 23:12:44 -0300

    atualiando gitignore

--- banner/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-class BannerAdmin(admin.ModelAdmin):
-    list_display = ('alt_text', 'image_tag_path')
-
-admin.site.register(Banner, BannerAdmin)
